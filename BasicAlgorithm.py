e5a3d6f3a7245e4987ce14c1aac378939c2453b8
Adong <jhdong8855@163.com>
2024-08-02 17:03:20 +0800

    相向双指针学习： 双数之和、三数之和、四数之和

--- BasicAlgorithm.py
+++ BasicAlgorithm.py
@@ -2,15 +2,78 @@ from typing import List
 from collections import defaultdict
 import time
 class Solution:
-    # 哈希(字典)
+    # 相向双指针
 
-    # 两数之和
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
-        for i, x in enumerate(nums):
-            for j in range(i + 1, len(nums)):
-                if x + nums[j] == target:
-                    return [i, j]
+    # 两数之和(有序情况 双指针)
+    def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        left, right = 0, len(numbers) - 1
+        while left < right:
+            if numbers[left] + numbers[right] == target:
+                return [left + 1, right + 1]
+            elif numbers[left] + numbers[right] < target:
+                left += 1
+            else:
+                right -= 1
 
+    # 三数之和
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        nums.sort()
+        res = []
+        for i in range(len(nums) - 2):
+            target = nums[i]
+            if i > 0 and nums[i] == nums[i - 1]:
+                continue
+            left, right = i + 1, len(nums) - 1
+            while left < right:
+                x = nums[left] + nums[right] + nums[i]
+                if x == 0:
+                    res.append([nums[i], nums[left], nums[right]])
+                    left += 1
+                    right -= 1
+                    while left < right and nums[left] == nums[left - 1]:
+                        left += 1
+                    while left < right and nums[right] == nums[right + 1]:
+                        right -= 1
+                elif x < 0:
+                    left += 1
+                else:
+                    right -= 1
+        return res
+
+    # 四数之和
+    def fourSum(self, nums: List[int], target: int) -> List[List[int]]:
+        nums.sort()
+        res = []
+        for i in range(len(nums) - 3):
+            if i > 0 and nums[i] == nums[i - 1]:
+                continue
+            for j in range(i + 1, len(nums) - 2):
+                if j > i + 1 and nums[j] == nums[j - 1]:
+                    continue
+                left, right = j + 1, len(nums) - 1
+                while left < right:
+                    x = nums[i] + nums[j] + nums[left] + nums[right]
+                    if x == target:
+                        res.append([nums[i], nums[j], nums[left], nums[right]])
+                        left += 1
+                        right -= 1
+                        while left < right and nums[left] == nums[left - 1]:
+                            left += 1
+                        while left < right and nums[right] == nums[right + 1]:
+                            right -= 1
+                    elif x < target:
+                        left += 1
+                    else:
+                        right -= 1
+        return res
+
+    # 统计和小于目标的下标对数目 TODO
+    # def countPairs(self, nums: List[int], target: int) -> int:
+
+
+
+
+# Test 打印耗费时间
 s = Solution()
 b = int(time.time() * 1000)
 # print(int(time.time() * 1000))
